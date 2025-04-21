42c61ca1eb9ff43f3e4802b7e14072ccfeccc25e
CattaRappa <sambow23@gmail.com>
2025-04-20 20:52:44 -0500

    Update applypatch.py
    Changes are from @0xNULLderef, just putting it here for the launcher until it either gets merged to BlueAmulet's repo or I update the launcher.

--- applypatch.py
+++ applypatch.py
@@ -36,21 +36,27 @@ patches32 = {
 
 # Incomplete Garry's Mod 64bit patches
 patches64 = {
-# 'bin/win64/engine.dll': [
-#     [('4883ec480f10', 0), '31c0c3'], #c_frustumcull patches
-#     [('753cf30f10', 0), 'eb'], # brush entity backfaces
-#     [('7e5244', 0), 'eb'], # world backfaces
-#     [('753c498b4204', 0), 'eb'], # world backfaces
-# ],
+'bin/win64/engine.dll': [
+    [('4883ec480f10', 0), '31c0c3'], # c_frustumcull, uses sse instructions in 64bit
+    [('753cf30f10', 0), 'eb'], # brush entity backfaces
+    [('7e5244', 0), 'eb'], # world backfaces (somewhat broken, check sort list size?)
+    [('753c498b4204', 0), 'eb'], # world backfaces
+    [('ff90????????85c0742b', 0), 'b80200000090'], # Mod_LoadLighting HDR
+    [('ff90????????85c00f84fa000000', 0), 'b80200000090'], # Mod_LoadWorldlights HDR
+    [('4883ec48803d??????????48', 0), '31c0c3'], # R_CullNode
+],
 'bin/win64/shaderapidx9.dll': [
     [('480f4ec1c7', 0), '90909090'], # four hardware lights
     [('4833cce8????03004881c448', 0), '85c0750466b80400'], # zero sized buffer
     [('4883ec084c', 0), '31c0c3'] # shader constants
 ],
-# 'bin/win64/client.dll': [
-#     [('4883ec480f1022', 0), '31c0c3'], # c_frustumcull
-#     [('0fb68154', 0), 'b001c3'], # r_forcenovis [getter]
-# ],
+'bin/win64/client.dll': [
+    [('4883ec480f1022', 0), '31c0c3'], # c_frustumcull
+    [('0fb68154', 0), 'b001c3'], # r_forcenovis [getter]
+],
+'bin/datacache.dll': [
+    [('647838302e767478', 0), '647839302e767478'], # force load dx9 vtx
+],
 'bin/win64/materialsystem.dll': [
     [('f77c24683bc10f4fc1488b8c24300100004833cce8????04004881c448010000', 0), '448b4424684585c0740341f7f839c80f4fc14881c448010000c3'], # zero sized buffer protection
 ],
