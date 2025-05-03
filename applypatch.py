0ca8d736f2d29f70e248ed8bdabf0df07127567d
CattaRappa <sambow23@gmail.com>
2025-05-03 09:47:44 -0500

    patcher: fix x64 culling patch
    made the signature more unique so we don't find multiple matches

--- applypatch.py
+++ applypatch.py
@@ -39,14 +39,14 @@ patches64 = {
 'bin/win64/engine.dll': [
     [('4883ec480f10', 0), '31c0c3'], # c_frustumcull, uses sse instructions in 64bit
     [('753cf30f10', 0), 'eb'], # brush entity backfaces
-    [('7e5244', 0), 'eb'], # world backfaces (somewhat broken, check sort list size?)
+    [('7e52448bd3', 0), 'eb'], # world backfaces (somewhat broken, check sort list size?)                           (FIX THIS ONE)
     [('753c498b4204', 0), 'eb'], # world backfaces
     [('ff90????????85c0742b', 0), 'b80200000090'], # Mod_LoadLighting HDR
     [('ff90????????85c00f84fa000000', 0), 'b80200000090'], # Mod_LoadWorldlights HDR
     [('4883ec48803d??????????48', 0), '31c0c3'], # R_CullNode
 ],
 'bin/win64/shaderapidx9.dll': [
-    [('480f4ec1c7', 0), '90909090'], # four hardware lights
+#    [('480f4ec1c7', 0), '90909090'], # four hardware lights          (This signature has completely changed and I have no idea how to fix it, so we're disabling it for now -CR)
     [('4833cce8????03004881c448', 0), '85c0750466b80400'], # zero sized buffer
     [('4883ec084c', 0), '31c0c3'] # shader constants
 ],
